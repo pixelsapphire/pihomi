b1a560fe41408ae0c9695f269d2a1e8d6435252c
pixelsapphire <rubynaxela@gmail.com>
2024-01-23 16:57:52 +0100

    Cleanup

--- github/gui.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/kasiarog/Embedded_systems-PiHoMi
-rm -rf ../gui/*
-mv Embedded_systems-PiHoMi/* ../gui
-rm ../gui/README.md
-rm -rf Embedded_systems-PiHoMi
--- github/server.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/x-yoshida/pihomi-server
-rm -rf ../server/src/*
-mv pihomi-server/src/* ../server/src
-rm -rf pihomi-server
