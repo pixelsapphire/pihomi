a8474dad02b4d14cbb20ff1a581d462322f81ad1
pixelsapphire <rubynaxela@gmail.com>
2024-01-23 19:39:21 +0100

    Add conditional compilation to gpio.cpp

--- server/src/gpio.cpp
+++ server/src/gpio.cpp
@@ -1,4 +1,3 @@
-#include <pigpiod_if2.h>
 #include <stdexcept>
 #include <string>
 #include "gpio.hpp"
@@ -7,6 +6,12 @@ namespace phm::gpio {
     int descriptor = -1;
 }
 
+phm::gpio::pin::pin(uint8_t number) : number(number) {}
+
+#ifdef RASPBERRY
+
+#include <pigpiod_if2.h>
+
 void phm::gpio::begin() {
     std::system("sudo pigpiod; sleep 1;");
     if ((phm::gpio::descriptor = pigpio_start(nullptr, nullptr)) < 0)
@@ -14,10 +19,16 @@ void phm::gpio::begin() {
                                   std::string(pigpio_error(phm::gpio::descriptor)));
 }
 
-phm::gpio::pin::pin(uint8_t number) : number(number) {}
-
 void phm::gpio::pin::set(phm::logic_state state) {
     if (gpio_write(phm::gpio::descriptor, number, state) < 0)
         throw std::runtime_error("Failed to change pin state: " +
                                  std::string(pigpio_error(phm::gpio::descriptor)));
 }
+
+#else
+
+void phm::gpio::begin() { phm::gpio::descriptor = 0; }
+
+void phm::gpio::pin::set(phm::logic_state state) {}
+
+#endif
