83799cde2bb61704428cca41090301b18e58e6fe
pixelsapphire <rubynaxela@gmail.com>
2024-01-23 16:52:29 +0100

    Add GPIO pins controller

---
+++ server/src/gpio.hpp
@@ -0,0 +1,38 @@
+#ifndef PIHOMI_GPIO_HPP
+#define PIHOMI_GPIO_HPP
+
+#include <cstdlib>
+#include <cstdint>
+
+namespace phm {
+
+    enum logic_state {
+        low = 0, high = 1
+    };
+
+    namespace gpio {
+
+        void begin();
+
+        class pin {
+
+            uint8_t number;
+
+        public:
+
+            explicit pin(uint8_t number);
+
+            pin(const pin&) = default;
+
+            pin(pin&&) noexcept = default;
+
+            pin& operator=(const pin&) = default;
+
+            pin& operator=(pin&&) noexcept = default;
+
+            void set(phm::logic_state state);
+        };
+}
+}
+
+#endif // PIHOMI_GPIO_HPP
